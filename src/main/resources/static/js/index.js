1bc38e7ca26bc7c16f20d8cb40855f36bff41bcb
yourisev <sevgama44@gmail.com>
2023-09-18 19:41:27 -0400

    Implemented a function to display connected users and another function to load connected users that calls the display function

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,43 @@
+function loadAndDisplayUsers() {
+
+    // check if the user is connected
+    const connectedUser = localStorage.getItem('connectedUser');
+    if (!connectedUser) {
+        window.location = 'login.html';
+        return;
+    }
+
+
+    const userListElement = document.getElementById("userList");
+    // Clear any existing content in the userListElement
+    userListElement.innerHTML = "Loading...";
+    // Retrieve the userList from Local Storage
+    fetch('http://localhost:8080/api/v1/users')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            displayUsers(data, userListElement);
+        });
+}
+
+function displayUsers(userList, userListElement) {
+    userListElement.innerHTML = "";
+
+    // Loop through the userList and create list items to display each user
+    userList.forEach(user => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+                <div>
+                    <i class="fa fa-user-circle"></i>
+                    ${user.username} <i class="user-email">(${user.email})</i>
+                </div>
+                <i class="fa fa-lightbulb-o ${user.status === "online" ? "online" : "offline"}"></i>
+            `;
+        userListElement.appendChild(listItem);
+    });
+}
+
+// Call the loadAndDisplayUsers function when the page loads
+window.addEventListener("load", loadAndDisplayUsers);
